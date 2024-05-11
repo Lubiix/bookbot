d31dfefee8b31a792350e5dfa58f0db83e7ef58e
Maxime Violi <maxime.violi@gmail.com>
2024-05-11 21:46:58 +0200

    final

--- main.py
+++ main.py
@@ -2,9 +2,17 @@ def main():
     book = "books/frankenstein.txt"
     text = get_book_text(book)
     nb_words = count_words(text)
-    print(f"There are {nb_words} dans {book}")
     list_chars = count_char(text)
-    print(list_chars)
+    sorted_list_chars = print_report(list_chars)
+    print(f"--- Begin report of {book} ---")
+    print(f"{nb_words} words found in the document")
+    print()
+    for item in sorted_list_chars:
+        if not item["char"].isalpha():
+            continue
+        print(f"The '{item['char']}' character was found {item['num']} times")
+
+    print("--- End report ---")
 
 def count_words(text):
     count = len(text.split())
@@ -24,7 +32,19 @@ def count_char(text):
         else:
             count_char[char] = 1
     return count_char
+
+def sort_on(dict):
+    return dict["num"]
+ 
+def print_report(dict_char):
+    list_char = []
+    for char, value in dict_char.items():
+        if char.isalpha():
+            list_char.append({"char":char, "num":value})
+    list_char.sort(reverse=True, key=sort_on)
     
+    return list_char
+        
 
 main()
 
